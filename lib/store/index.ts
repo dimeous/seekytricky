47effe6248aac67e267ed5ac972404febf9f6e71
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2025-11-06 09:37:50 +0300

    start button ready

--- lib/store/index.ts
+++ lib/store/index.ts
@@ -5,7 +5,7 @@ export const store = configureStore({
   reducer: {
     game,
   },
-});
+})
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
